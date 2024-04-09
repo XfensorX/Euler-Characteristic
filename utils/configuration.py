f2fed55cb38cecc48302b0b6f7065f54aef9144f
XfensorX <97126540+XfensorX@users.noreply.github.com>
2024-04-08 23:27:56 -1000

    refactor configuration

--- utils/configuration.py
+++ utils/configuration.py
@@ -9,9 +9,45 @@ import yaml
 class Criterion(enum.Enum):
     MSE_LOSS = "MSELoss"
 
+    def __str__(self):
+        return self.value
+
+    def get_func(self):
+        if self is Criterion.MSE_LOSS:
+            return torch.nn.MSELoss()
+
+    @staticmethod
+    def check_if_valid(criterion: str):
+        if criterion in [c.value for c in Criterion]:
+            return
+
+        error = ValueError("Invalid criterion in configuration.")
+        error.note = "Valid criteria are: \n- " + "\n- ".join(
+            [c.value for c in Criterion]
+        )
+        raise error
+
 
 class Optimizer(enum.Enum):
     ADAM = "Adam"
+
+    def __str__(self):
+        return self.value
+
+    def get_func(self, model_parameters, learning_rate):
+        if self is Optimizer.ADAM:
+            return torch.optim.Adam(model_parameters, lr=learning_rate)
+
+    @staticmethod
+    def check_if_valid(optimizer: str):
+        if optimizer in [o.value for o in Optimizer]:
+            return
+
+        error = ValueError("Invalid optimizer in configuration.")
+        error.note = "Valid optimizer are: \n- " + "\n- ".join(
+            [o.value for o in Optimizer]
+        )
+        raise error
 
 
 @dataclass
@@ -28,40 +64,17 @@ class ExperimentConfig:
     criterion: Criterion
     optimizer: Optimizer
 
-    def get_criterion_func(self):
-        self.check_for_correct_criterion()
+    def __post_init__(self):
+        Optimizer.check_if_valid(self.optimizer)
+        Criterion.check_if_valid(self.criterion)
+        self.optimizer = Optimizer(self.optimizer)
+        self.criterion = Criterion(self.criterion)
 
-        if self.criterion == Criterion.MSE_LOSS.value:
-            return torch.nn.MSELoss()
+    def get_criterion_func(self):
+        self.criterion.get_func()
 
     def get_optimizer_func(self, model_parameters):
-        self.check_for_correct_optimizer()
-
-        if self.optimizer == Optimizer.ADAM.value:
-            return torch.optim.Adam(model_parameters, lr=self.learning_rate)
-
-    def check_for_correct_optimizer(self):
-        if self.optimizer in [e.value for e in Optimizer]:
-            return
-        error = ValueError("Invalid optimizer in configuration.")
-        error.note = "Valid optimizer are: \n- " + "\n- ".join(
-            [o.value for o in Optimizer]
-        )
-        raise error
-
-    def check_for_correct_criterion(self):
-        if self.criterion in [e.value for e in Criterion]:
-            return
-
-        error = ValueError("Invalid criterion in configuration.")
-        error.note = "Valid criteria are: \n- " + "\n- ".join(
-            [c.value for c in Criterion]
-        )
-        raise error
-
-    def __post_init__(self):
-        self.check_for_correct_criterion()
-        self.check_for_correct_optimizer()
+        self.optimizer.get_func(model_parameters, self.learning_rate)
 
     def __str__(self):
         config_dict = {field: getattr(self, field) for field in self.__annotations__}
@@ -74,10 +87,6 @@ class ExperimentConfig:
 def load_config(experiment_dir: str, config_file: str) -> ExperimentConfig:
     """
     Load experiment configuration from a YAML file.
-
-    :param experiment_dir: Directory containing experiment configuration
-    :param config_file: Experiment configuration file
-    :return: Experiment configuration
     """
     config_path = os.path.join(experiment_dir, config_file)
     with open(config_path, "r") as f:
