e696d572d18689a88c2a76564caf3ef4cde40d87
XfensorX <97126540+XfensorX@users.noreply.github.com>
2024-04-16 14:17:46 -1000

    add gpu support and more variable support to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,19 @@
+#TODO: Refactor
+
+
+variable "REGION" {
+  type    = string
+  default = "us-west1"
+}
+
+variable "ZONE" {
+  type    = string
+  default = "a"
+}
+
 provider "google" {
   project = "idyllic-chimera-419902"
   region  = "us-west1"
-}
-
-variable "CPUs" {
-  type    = number
-  default = 2
 }
 
 variable "NAME" {
@@ -13,20 +21,31 @@ variable "NAME" {
   description = "The name of the VM"
 }
 
+variable "GPU_TYPE" {
+  type    = string
+  default = "nvidia-test-t4"
+}
+variable "GPU_COUNT" {
+  type    = number
+  default = 1
+}
+
+variable "CPUs" {
+  type    = number
+  default = 2
+}
 
 variable "MEMORY" {
   type    = number
   default = 4096
 }
 
-variable "USE_CUDA" {
-  type    = bool
-  default = false
-
+variable "USE_GPU" {
+  type = bool
 }
 
 data "google_compute_image" "used_image" {
-  family  = "pytorch-latest-cpu"
+  family  = var.USE_GPU ? "pytorch-latest-gpu" : "pytorch-latest-cpu"
   project = "deeplearning-platform-release"
 }
 
@@ -35,8 +54,8 @@ resource "google_compute_instance" "vm_instance" {
 
   name         = var.NAME
   description  = "Used for computing euler characteristics"
-  machine_type = "custom-${var.CPUs}-${var.MEMORY}"
-  zone         = "us-west1-a"
+  machine_type = var.USE_GPU ? "n1-standard-1" : "custom-${var.CPUs}-${var.MEMORY}"
+  zone         = "${var.REGION}-${var.ZONE}"
 
   boot_disk {
     initialize_params {
@@ -49,4 +68,18 @@ resource "google_compute_instance" "vm_instance" {
     access_config {}
   }
   allow_stopping_for_update = true
+  metadata = {
+    "install-nvidia-driver" = var.USE_GPU ? true : false
+  }
+
+  guest_accelerator = var.USE_GPU ? [{
+    type  = var.GPU_TYPE
+    count = var.GPU_COUNT
+  }] : null
+
+  scheduling {
+    on_host_maintenance = "TERMINATE"
+  }
+
+
 }
