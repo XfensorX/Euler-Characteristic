01e021aeef2b92c1c9a3c711e1874755ad2377a0
JiWa2000 <jimi2000@web.de>
2024-05-28 09:21:39 +0200

    .

--- experiments/transformer/train.py
+++ experiments/transformer/train.py
@@ -10,12 +10,12 @@ from models.VanillaVTrans import VisionTransformer
 def get_models(config: ExperimentConfig):
     return {
         "transformer": Transformer(
-            embed_size=128,
+            embed_size=144,
             output_size=1,
             img_x_size=config.img_x_size,
             img_y_size=config.img_y_size,
-            heads=8,  # You might also want to make this configurable
-            num_layers=8  # Set this as per your model complexity requirement
+            heads=4,  # You might also want to make this configurable
+            num_layers=16  # Set this as per your model complexity requirement
         )
     }
 
